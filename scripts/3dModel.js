a54491d49ec45d43c2c33b73e8103d43029f6d00
TearsOprah <qmeshokp@mail.ru>
2023-08-30 22:25:22 +0300

    feat: отрисовка точек из points.json

--- scripts/3dModel.js
+++ scripts/3dModel.js
@@ -1,16 +1,30 @@
 const viewer = new Cesium.Viewer("cesiumContainer");
 
-const longitude = -75.59777;
-const latitude = 40.03883;
-const height = 100;
+fetch('./data/points.json')
+  .then(response => response.json())
+  .then(jsonData => {
+    jsonData.forEach(pointData => {
+      const longitude = pointData.longitude;
+      const latitude = pointData.latitude;
+      const height = 0;
 
-const position = Cesium.Cartesian3.fromDegrees(longitude, latitude, height);
+      const position = Cesium.Cartesian3.fromDegrees(longitude, latitude, height);
 
-// Создание точки
-const point = viewer.entities.add({
-  position: position,
-  point: {
-    pixelSize: 10,
-    color: Cesium.Color.RED
-  }
-});
+      viewer.entities.add({
+        position: position,
+        point: {
+          pixelSize: 10,
+          color: Cesium.Color.GREEN
+        },
+        label: {
+          text: pointData.label,
+          font: "14pt monospace",
+          style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+          outlineWidth: 2,
+          verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+          pixelOffset: new Cesium.Cartesian2(0, -9),
+        }
+      });
+    });
+  })
+  .catch(error => console.error('Error loading JSON:', error));
